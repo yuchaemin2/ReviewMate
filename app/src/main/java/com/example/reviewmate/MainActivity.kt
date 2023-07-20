2be6b646a8f9e88dbb617b97d13eb3cd5a22359c
yuchaemin2 <kusuri94@gmail.com>
2023-07-20 15:26:40 +0900

    test3

--- app/src/main/java/com/example/reviewmate/MainActivity.kt
+++ app/src/main/java/com/example/reviewmate/MainActivity.kt
@@ -4,10 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    var mode = "test3"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-
 
 }
